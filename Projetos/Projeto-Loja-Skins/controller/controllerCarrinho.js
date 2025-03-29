22712040972c1f385498876f5e2bd8c0ff7b1077
jailsonneve <daiarthur053@gmail.com>
2025-03-28 22:25:49 -0300

    update 2.4

---
+++ Projetos/Projeto-Loja-Skins/controller/controllerCarrinho.js
@@ -0,0 +1,22 @@
+function verCarrinho() {
+    if (carrinho.length === 0) {
+      Swal.fire("Carrinho vazio", "Nenhum item adicionado ainda.", "info");
+      return;
+    }
+
+    let html = "<ul style='text-align:left;'>";
+    let total = 0;
+
+    carrinho.forEach(item => {
+      html += `<li><strong>${item.nome}</strong> - R$ ${item.preco.toFixed(2)}</li>`;
+      total += item.preco;
+    });
+
+    html += "</ul>";
+
+    Swal.fire({
+      title: "Itens no Carrinho",
+      html: html + `<hr><strong>Total: R$ ${total.toFixed(2)}</strong>`,
+      confirmButtonText: "Fechar"
+    });
+  }
